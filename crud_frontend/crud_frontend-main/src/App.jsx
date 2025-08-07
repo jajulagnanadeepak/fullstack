4676566178aa16c6f5d643267deb60b9c0049f10
jajulagnanadeepak <163728519+jajulagnanadeepak@users.noreply.github.com>
2025-08-08 01:38:46 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -49,7 +49,7 @@ const BASE_URL = 'http://13.61.150.180:9090/springapp1';
  
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
-      await axios.delete(`http://localhost:9090/springapp1/delete/${id}`);
+      await axios.delete(`http://13.61.150.180:9090/springapp1/delete/${id}`);
       alert('Delete successful');
       fetchProducts();
     }
